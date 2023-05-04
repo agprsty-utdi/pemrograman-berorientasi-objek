61aad952ffe2fa1914d0893909ba18c03a0c821e
agprsty-utdi <agung.prasetyo@students.utdi.ac.id>
2023-05-04 19:50:18 +0700

    praktikum pertemuan 7

---
+++ src/tujuh/TestPewarisan.java
@@ -0,0 +1,13 @@
+package tujuh;
+
+public class TestPewarisan{
+  public static void main(String args[])
+  {
+    Manusia orang1=new Manusia();
+    orang1.cetak();
+    System.out.println();
+    System.out.println();
+    Mahasiswa mhs1=new Mahasiswa();
+    mhs1.cetak();
+  }
+}
