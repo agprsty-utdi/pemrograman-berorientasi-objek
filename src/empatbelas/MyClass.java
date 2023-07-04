8710fa8cb0ab23e732ca9e3f404c2969f4feb431
agung <agung@digitalsekuriti.id>
2023-07-04 13:29:58 +0700

    praktikum pertemuan 14

---
+++ src/empatbelas/MyClass.java
@@ -0,0 +1,14 @@
+package empatbelas;
+
+public class MyClass{
+  public MyClass(){
+  new Thread(){
+    public void run(){
+      System.out.println("Threat dijalanankan ");
+    }
+    }.start();
+  }
+  public static void main(String[] args){
+  new MyClass ();
+  }
+}
